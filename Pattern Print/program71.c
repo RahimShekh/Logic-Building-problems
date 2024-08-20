4b838b26b7d758702251361fef2de391ff593b7a
RahimShekh <rahimshekh46751@gmail.com>
2024-08-20 12:56:05 +0530

    Add files via upload

---
+++ Pattern Print/program71.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void Display(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == j)
+            {
+                printf("1\t");
+            }
+            else
+            {
+                printf("$\t");
+            }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter the Rows:");
+    scanf("%d",&iValue1);
+
+    printf("Enter the columns:");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+    return 0;
+}
